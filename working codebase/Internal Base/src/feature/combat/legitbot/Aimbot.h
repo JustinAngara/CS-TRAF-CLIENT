5b613ab777f517f5f75d6c16d6121e69fde74359
Justin Angara <justin.angara@gmail.com>
2026-01-07 14:00:34 -0600

    miiiiiiiiiiiiiiiiiiiiiiin

--- working codebase/Internal Base/src/feature/combat/legitbot/Aimbot.h
+++ working codebase/Internal Base/src/feature/combat/legitbot/Aimbot.h
@@ -4,9 +4,9 @@
 
 namespace Aimbot
 {
-	// Settings
+	// settings
 	inline bool aimbot_enabled = true;
-	inline int aimbot_key = VK_F2; // Right mouse button
+	inline int aimbot_key = VK_LBUTTON; 
 	inline float aimbot_fov = 15.0f;
 	inline bool aimbot_smooth = true;
 	inline float aimbot_smoothness = 0.1f;
